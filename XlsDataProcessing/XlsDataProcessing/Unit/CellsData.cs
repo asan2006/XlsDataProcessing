2f4b25040c5de8d32b7a281f2841549877d0fab8
Robin <bin___03@163.com>
2017-04-21 23:36:11 +0800

    先用增加的单元格数据类，后续再补充数据有效性的验证

---
+++ XlsDataProcessing/XlsDataProcessing/Unit/CellsData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XlsDataProcessing.Unit
+{
+    class CellsData
+    {
+        public string cellName
+        {
+            get;
+            set;
+        }
+
+        public string cellValue
+        {
+            get;
+            set;
+        }
+    }
+}
